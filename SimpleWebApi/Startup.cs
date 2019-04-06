7266c98f6453d4cfb94aac24e54378ad1f600225
tamtamyarn <tamtamyarn@outlook.com>
2019-04-06 09:06:03 +0900

    Enable Swagger/OpenAPI.

--- SimpleWebApi/Startup.cs
+++ SimpleWebApi/Startup.cs
@@ -5,6 +5,8 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using SimpleWebApi.Data;
+using NJsonSchema;
+using NSwag.AspNetCore;
 
 namespace SimpleWebApi
 {
@@ -21,6 +23,7 @@ namespace SimpleWebApi
         {
             services.AddDbContext<BookContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddOpenApiDocument();
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -33,6 +36,9 @@ namespace SimpleWebApi
             {
                 app.UseHsts();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUi3();
 
             app.UseHttpsRedirection();
             app.UseMvc();
