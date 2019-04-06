b5ac8580bb67232437d81176fadbb1f5d7436ec9
tamtamyarn <tamtamyarn@outlook.com>
2019-04-07 00:55:07 +0900

    Add Post, Put, Delete.

--- SimpleWebApi/Controllers/BooksController.cs
+++ SimpleWebApi/Controllers/BooksController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using SimpleWebApi.Data;
 using SimpleWebApi.Entities;
 
@@ -25,10 +26,47 @@ namespace SimpleWebApi.Controllers
         }
 
         [HttpGet("{id}")]
-        public ActionResult<Book> Get(int id)
+        public ActionResult<Book> GetById(int id)
         {
             var book = context.Books.FirstOrDefault(b => b.BookId.Equals(id));
             return book;
+        }
+
+        [HttpPost]
+        public ActionResult<Book> Post(Book book)
+        {
+            context.Books.Add(book);
+            context.SaveChanges();
+            return CreatedAtAction(nameof(GetById), new { id = book.BookId }, book);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, Book book)
+        {
+            if (id != book.BookId)
+            {
+                return BadRequest();
+            }
+
+            context.Entry(book).State = EntityState.Modified;
+            context.SaveChanges();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var book = context.Books.FirstOrDefault(b => b.BookId.Equals(id));
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            context.Books.Remove(book);
+            context.SaveChanges();
+
+            return NoContent();
         }
     }
 }
