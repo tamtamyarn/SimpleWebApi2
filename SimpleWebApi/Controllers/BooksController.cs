7fe218276f7c39b206a8d9a8cc197da0b79b5907
tamtamyarn <tamtamyarn@outlook.com>
2019-04-11 01:19:53 +0900

    Fix

--- SimpleWebApi/Controllers/BooksController.cs
+++ SimpleWebApi/Controllers/BooksController.cs
@@ -24,14 +24,14 @@ namespace SimpleWebApi.Controllers
             var books = context.Books;
             return books;
         }
-
+        
         [HttpGet("{id}")]
         public ActionResult<Book> Get(int id)
         {
             var book = context.Books.FirstOrDefault(b => b.BookId.Equals(id));
             return book;
         }
-
+        
         [HttpPost]
         public ActionResult<Book> Create(Book book)
         {
@@ -39,29 +39,19 @@ namespace SimpleWebApi.Controllers
             context.SaveChanges();
             return CreatedAtAction(nameof(Get), new { id = book.BookId }, book);
         }
-
-        [HttpPut("{id}")]
-        public IActionResult Update(int id, Book book)
+        
+        [HttpPut]
+        public IActionResult Update(Book book)
         {
-            if (id != book.BookId)
-            {
-                return BadRequest();
-            }
-
             context.Entry(book).State = EntityState.Modified;
             context.SaveChanges();
             return NoContent();
         }
-
+        
         [HttpDelete("{id}")]
         public IActionResult Delete(int id)
         {
             var book = context.Books.FirstOrDefault(b => b.BookId.Equals(id));
-
-            if (book == null)
-            {
-                return NotFound();
-            }
 
             context.Books.Remove(book);
             context.SaveChanges();
--- SimpleWebApi/Startup.cs
+++ SimpleWebApi/Startup.cs
@@ -5,7 +5,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using SimpleWebApi.Data;
-using NJsonSchema;
 using NSwag.AspNetCore;
 
 namespace SimpleWebApi
